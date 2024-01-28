9aa1651b7ddf79c7d39a5f65f40ed4bf1f76f16e
vaageeshadas <vaageesha.das@gmail.com>
2024-01-27 19:20:58 -0500

    map set up!

--- lib/Views/ExplorePageView.dart
+++ lib/Views/ExplorePageView.dart
@@ -3,69 +3,38 @@ import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_mapbox_navigation/library.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+// import 'package:latlong2/latlong.dart';
+import 'package:latlong2/latlong.dart' as latlong;
+import 'package:url_launcher/url_launcher.dart';
 
 
-/*
 class ExplorePageView extends StatefulWidget {
   @override
   _ExplorePageViewState createState() => _ExplorePageViewState();
 }
 
 class _ExplorePageViewState extends State<ExplorePageView> {
-  late MapboxMapController mapController;
-
-  // Use dotenv to get the MAPBOX_ACCESS_TOKEN
-  final String mapboxAccessToken = dotenv.env['MAPBOX_DOWNLOADS_TOKEN'] ?? '';
-
-
-  @override
-  void initState() {
-    super.initState();
-    // If needed, initialize your token here
-  }
-
-  void _onMapCreated(MapboxMapController controller) {
-    mapController = controller;
-    // Add additional initialization if necessary, like setting the style
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: MapboxMap(
-        accessToken: mapboxAccessToken, // Pass the access token here
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(0.0, 0.0), // Starting point of the map
-          zoom: 2.0, // Zoom level
-        ),
-        // Add more customization to your map below
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: latlong.LatLng(39.8283, -98.5795), // Near the geographic center of the U.S.
+        initialZoom: 3, // A lower zoom level to show more of the map
       ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+            ),
+          ],
+        ),
+      ],
     );
   }
-}*/
-
-@override
-Widget build(BuildContext context) {
-  return FlutterMap(
-    options: MapOptions(
-      initialCenter: LatLng(51.509364, -0.128928),
-      initialZoom: 9.2,
-    ),
-    children: [
-      TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'com.example.app',
-      ),
-      RichAttributionWidget(
-        attributions: [
-          TextSourceAttribution(
-            'OpenStreetMap contributors',
-            onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-          ),
-        ],
-      ),
-    ],
-  );
 }
