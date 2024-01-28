0dd1a118049c7a784aa42de1b0863dbf0c29bf6d
Dhwani <dhwani.kanani17@gmail.com>
2024-01-27 23:19:07 -0500

    Add in logo

--- lib/logininpagetry.dart
+++ lib/logininpagetry.dart
@@ -24,90 +24,88 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _passwordController = TextEditingController();
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Welcome Back!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold, // Make it bold
-              fontSize: 35.0, // Increase font size
-            ),
-          ),
-          SizedBox(height: 8), // Add space between titles
-          Text(
-            'Log In', 
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold, // Make it bold
-              fontSize: 30.0, // Increase font size
-            ),
-          ),
-        ],
-      ),
-      centerTitle: true, // Center align the titles
-      toolbarHeight: 120.0, // Adjust the height of the AppBar
-      backgroundColor: Colors.blue, // Set background color of the toolbar
-    ),
-    backgroundColor: Colors.white, // Change the background color of the body
-    body: Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-           FractionallySizedBox(
-              widthFactor: 1.0, // Full width
-              heightFactor: 0.3, // Half of the screen height
-              child: Image.asset(
-                'assets/Login_Image.jpg', // Replace with your actual image file name and extension
-                fit: BoxFit.cover,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome Back!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 35.0,
               ),
             ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-                labelStyle: TextStyle(color: Colors.white), // Set label text color
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.white), // Set label text color
-              ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                // Replace this with your authentication logic
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-
-                // For simplicity, just print the credentials for now
-                print('Username: $username');
-                print('Password: $password');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue, // Set background color of the button
-              ),
-              child: Text(
-                'Login',
-                style: TextStyle(color: Colors.white), // Set text color of the button
+            SizedBox(height: 8),
+            Text(
+              'Log In',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
               ),
             ),
           ],
         ),
+        centerTitle: true,
+        toolbarHeight: 150.0,
+        backgroundColor: Color.fromARGB(255, 180, 96, 240),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Add the logo using the Image widget below the toolbar
+          Image.asset(
+            'assets/Logo1.png',
+            height: 250,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    // Replace this with your authentication logic
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
+
+                    // For simplicity, just print the credentials for now
+                    print('Username: $username');
+                    print('Password: $password');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 180, 96, 240),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
