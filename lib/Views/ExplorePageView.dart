88453c9a0e7935bbaf303963c4c4654a0b1c0f4d
dhiya r. <dhiyayr@gmail.com>
2024-01-27 23:26:59 -0500

    added pins

--- lib/Views/ExplorePageView.dart
+++ lib/Views/ExplorePageView.dart
@@ -3,64 +3,14 @@ import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_mapbox_navigation/library.dart';
 import 'package:flutter_map/flutter_map.dart';
-// import 'package:latlong2/latlong.dart';
 import 'package:latlong2/latlong.dart' as latlong;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:graffitiwall/main.dart';
-
 
 class ExplorePageView extends StatefulWidget {
   @override
   _ExplorePageViewState createState() => _ExplorePageViewState();
 }
-
-// class _ExplorePageViewState extends State<ExplorePageView> {
-//   late List<Marker> _markers;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _markers = [
-//       Marker(
-//         point: latlong.LatLng(39.8283, -98.5795), // Example coordinates
-//         width: 80.0,
-//         height: 80.0,
-//         builder: (ctx) => Container(
-//           child: Icon(Icons.location_pin, color: Colors.red, size: 40), // Marker icon with size
-//         ),
-//       ),
-//       // Add more markers as needed
-//     ];
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//   return FlutterMap(
-//     options: MapOptions(
-//       initialCenter: latlong.LatLng(39.8283, -98.5795), // Near the geographic center of the U.S.
-//       initialZoom: 3, // A lower zoom level to show more of the map
-//     ),
-//     children: [
-//       TileLayer(
-//         urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-//         userAgentPackageName: 'com.example.app',
-//       ),
-//       MarkerLayerOptions(
-//         markers: _markers,
-//       ),
-//       RichAttributionWidget(
-//         attributions: [
-//           TextSourceAttribution(
-//             'OpenStreetMap contributors',
-//             onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-//           ),
-//         ],
-//       ),
-//     ],
-//   );
-// }
-// }
-
 
 class _ExplorePageViewState extends State<ExplorePageView> {
   late List<Marker> _markers;
@@ -73,9 +23,8 @@ class _ExplorePageViewState extends State<ExplorePageView> {
         width: 80.0,
         height: 80.0,
         point: latlong.LatLng(39.8283, -98.5795), // Example coordinates
-        builder: (ctx) => Container(
-          child: Icon(Icons.location_pin, color: Colors.red), // Marker icon
-        ),
+        // Adjusted to a general widget
+        child: Icon(Icons.location_pin, color: Colors.red),
       ),
       // Add more markers as needed
     ];
@@ -88,12 +37,13 @@ class _ExplorePageViewState extends State<ExplorePageView> {
         center: latlong.LatLng(39.8283, -98.5795), // Initial center of the map
         zoom: 3, // Initial zoom level
       ),
-      layers: [
-        TileLayerOptions(
+      // Adjust the way layers are added
+      children: [
+        TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.example.app',
         ),
-        MarkerLayerOptions(
+        MarkerLayer(
           markers: _markers,
         ),
       ],
