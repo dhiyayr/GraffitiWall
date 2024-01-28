534c8cc444b73abfc9cc4c067291828538db2489
Dhwani <dhwani.kanani17@gmail.com>
2024-01-27 23:27:28 -0500

    added logo

--- lib/Views/LoginPageView.dart
+++ lib/Views/LoginPageView.dart
@@ -34,54 +34,86 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Welcome!\nCreate an Account',
-          textAlign: TextAlign.center,
-        ),
-        centerTitle: true, // Center align the title
-        titleSpacing: 30.0,
-        toolbarHeight: 150.0, 
-      ),
-      backgroundColor: Colors.black, // Change the background color to black
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
+        title: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-                labelStyle: TextStyle(color: Colors.white),
+            Text(
+              'Welcome Back!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 35.0,
               ),
             ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.white),
+            SizedBox(height: 8),
+            Text(
+              'Log In',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
               ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                // Replace this with your authentication logic
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-
-                // For simplicity, just print the credentials for now
-                print('Username: $username');
-                print('Password: $password');
-              },
-              child: Text('Login'),
             ),
           ],
         ),
+        centerTitle: true,
+        toolbarHeight: 150.0,
+        backgroundColor: Color.fromARGB(255, 180, 96, 240),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Add the logo using the Image widget below the toolbar
+          Image.asset(
+            'assets/Logo1.png',
+            height: 250,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    // Replace this with your authentication logic
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
+
+                    // For simplicity, just print the credentials for now
+                    print('Username: $username');
+                    print('Password: $password');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 180, 96, 240),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
